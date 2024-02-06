49ee45c2b2b0d9e2ee41facbb2efebfff9b3a18b
subhoku <career@codeographsolutions.com>
2024-02-06 19:21:19 +0530

    Today's Changes

--- Fragments/Fragments/src/ElectronicProducts.jsx
+++ Fragments/Fragments/src/ElectronicProducts.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import './App.css';
 function ElectronicProducts(){
  let electronics=["mobile", "pC", "laptop", "tablet","Luxury Products","Food"];
     return(
         <>
         <h1>Electronic Product List</h1>
-        <ul className="list-group">
+        <ul className="list-group" class="bg-color">
           {electronics.map((item, index) => (
             <li key={index} className="list-group-item">
               {item}
