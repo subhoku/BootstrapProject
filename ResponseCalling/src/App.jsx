b51546a5d465f028b6d1fe6cbaaec3f1f4e9316f
subhoku <career@codeographsolutions.com>
2024-02-19 19:27:52 +0530

    Changes Pushed Yestarday's

---
+++ ResponseCalling/src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import DataBinding from './products';
+
+function App() {
+ 
+ return (
+<DataBinding/>
+ );
+}
+
+export default App
---
+++ ResponseCalling/src/products.jsx
@@ -0,0 +1,42 @@
+function DataBinding() {
+  // Products array with 3 objects
+  var products = [
+    {
+      Name: "Samsung TV",
+      Price: 54323.23,
+    },
+    {
+      Name: "Sony",
+      Price: 154323.23,
+    },
+    {
+      Name: "Micromax",
+      Price: 24323.23,
+    },
+    {
+      Name: "Sony",
+      Price: 154323.23,
+    },
+    {
+      Name: "Videocon",
+      Price: 24323.23,
+    }
+  ];
+
+  // Use map to create an array of JSX elements
+  const productRows = products.map((product, index) => (
+    <div key={index} className="product-row">
+      <span className="product-name">{product.Name}</span>
+      <span className="product-price">${product.Price.toFixed(2)}</span>
+    </div>
+  ));
+
+  return (
+    <div className="container">
+      <h2>Product Table</h2>
+      {productRows}
+    </div>
+  );
+}
+
+export default DataBinding;
